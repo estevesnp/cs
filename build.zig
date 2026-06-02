471a5e54f74f066112cbeaf57644643f55d00396
estevesnp <nuno.pinto@live.com>
2026-06-02 10:16:50 +0100

    chore: bump zig version to 0.17.0-dev.644+3de725074

--- build.zig
+++ build.zig
@@ -28,16 +28,12 @@ pub fn build(b: *std.Build) !void {
 
     b.installArtifact(exe);
 
-    const run_step = b.step("run", "Run the app");
-
     const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
     run_cmd.step.dependOn(b.getInstallStep());
+    run_cmd.addPassthruArgs();
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 
     { // docs
         const docs_install = b.addInstallDirectory(.{
