965a8542b4a00b0d1da349f632722a81da1848aa
estevesnp <nuno.pinto@live.com>
2026-08-28 10:25:11 +0100

    feat: init parsing

--- build.zig
+++ build.zig
@@ -6,23 +6,20 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const options = b.addOptions();
-
-    const version = try std.SemanticVersion.parse(build_zig_zon.version);
-    options.addOption(std.SemanticVersion, "cs_version", version);
-
     const mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const options = b.addOptions();
+    options.addOption([]const u8, "cs_version", getCsVersion(b));
     mod.addOptions("options", options);
 
     const exe = b.addExecutable(.{
         .name = "cs",
         .root_module = mod,
-        .use_llvm = b.option(bool, "llvm", "Use llvm for executable"),
+        .use_llvm = b.option(bool, "llvm", "use llvm for executable"),
     });
 
     b.installArtifact(exe);
@@ -31,10 +28,10 @@ pub fn build(b: *std.Build) !void {
     run_cmd.step.dependOn(b.getInstallStep());
     run_cmd.addPassthruArgs();
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const filters = b.option([]const []const u8, "test-filter", "Test filters") orelse &.{};
+    const filters = b.option([]const []const u8, "test-filter", "test filters") orelse &.{};
     const exe_tests = b.addTest(.{
         .filters = filters,
         .root_module = b.createModule(.{
@@ -48,7 +45,7 @@ pub fn build(b: *std.Build) !void {
 
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
-    const test_step = b.step("test", "Run tests");
+    const test_step = b.step("test", "run tests");
     test_step.dependOn(&run_exe_tests.step);
 
     const check_exe = b.addExecutable(.{
@@ -56,7 +53,24 @@ pub fn build(b: *std.Build) !void {
         .root_module = mod,
     });
 
-    const check_step = b.step("check", "Check that app compiles");
+    const check_step = b.step("check", "check that app compiles");
     check_step.dependOn(&check_exe.step);
     check_step.dependOn(&exe_tests.step);
+}
+
+fn getCsVersion(b: *std.Build) []const u8 {
+    const tagged_version = b.option(bool, "tagged-version", "use tagged version") orelse false;
+    if (tagged_version) return build_zig_zon.version;
+
+    const res = b.runFallible(&.{ "git", "rev-parse", "--short", "HEAD" }, .{});
+    switch (res) {
+        .success => |hash| {
+            const trimmed = std.mem.trimEnd(u8, hash, "\r\n");
+            return b.fmt("{s}-dev.{s}", .{ build_zig_zon.version, trimmed });
+        },
+        else => {
+            std.log.err("error fetching git hash ({t}). defaulting to version from build.zig.zon", .{res});
+            return build_zig_zon.version;
+        },
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,96 @@
 const std = @import("std");
+const mem = std.mem;
+
 const Io = std.Io;
+const Allocator = std.mem.Allocator;
+
+const options = @import("options");
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
-    try Io.File.stdout().writeStreamingAll(io, "Hello, World (again)!\n");
+    const ctx: Ctx = .{
+        .io = init.io,
+        .gpa = init.gpa,
+        .arena = init.arena.allocator(),
+    };
+
+    const args = try init.minimal.args.toSlice(ctx.arena);
+
+    const cmd = try parseArgs(args);
+
+    switch (cmd) {
+        .version => try Io.File.stdout().writeStreamingAll(ctx.io, options.cs_version),
+        else => {},
+    }
 }
+
+const Ctx = struct {
+    io: Io,
+    gpa: Allocator,
+    arena: Allocator,
+};
+
+fn parseArgs(args: []const []const u8) CmdError!Cmd {
+    var it: Iter = .init(args[1..]);
+
+    while (it.next()) |arg| {
+        if (eqlAny(arg, &.{ "help", "--help", "-h" })) return CmdError.Help;
+        if (eqlAny(arg, &.{ "version", "--version", "-v" })) return .version;
+        if (mem.eql(u8, arg, "env")) return .env;
+
+        if (mem.eql(u8, arg, "search")) return .{ .search = try parseSearch(&it) };
+
+        if (!mem.eql(u8, arg, "--") and mem.startsWith(u8, arg, "-")) return CmdError.Usage;
+
+        // defaulting to search, so need to un-consume the arg
+        it.prev();
+        return .{ .search = try parseSearch(&it) };
+    }
+
+    // default to search when no args are passed
+    return .{ .search = try parseSearch(&it) };
+}
+
+fn parseSearch(it: *Iter) CmdError!SearchOpts {
+    std.debug.print("parsing search\n", .{});
+    while (it.next()) |arg| {
+        std.debug.print("  {s}\n", .{arg});
+    }
+    return CmdError.Help;
+}
+
+fn eqlAny(needle: []const u8, haystack: []const []const u8) bool {
+    for (haystack) |elem| if (mem.eql(u8, needle, elem)) return true;
+    return false;
+}
+
+const CmdError = error{ Help, Usage };
+
+const Cmd = union(enum) {
+    search: SearchOpts,
+    env,
+    version,
+};
+
+const SearchOpts = struct {
+    path: []const u8,
+};
+
+const Iter = struct {
+    slice: []const []const u8,
+    idx: usize,
+
+    fn init(slice: []const []const u8) Iter {
+        return .{ .idx = 0, .slice = slice };
+    }
+
+    fn next(self: *Iter) ?[]const u8 {
+        if (self.idx >= self.slice.len) return null;
+        const elem = self.slice[self.idx];
+        self.idx += 1;
+        return elem;
+    }
+
+    fn prev(self: *Iter) void {
+        if (self.idx > 0) self.idx -= 1;
+    }
+};
